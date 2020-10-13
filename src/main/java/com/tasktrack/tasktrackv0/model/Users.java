bdf09b0a0a43f596373a8ba3ef26b81542a58c47
jason <jasonkim4201@gmail.com>
2020-10-13 13:17:49 -0400

    Created user and a todo model. Added jpa annotations and and a many to one association in todos model.

--- src/main/java/com/tasktrack/tasktrackv0/model/Users.java
+++ src/main/java/com/tasktrack/tasktrackv0/model/Users.java
@@ -1,4 +1,57 @@
 package com.tasktrack.tasktrackv0.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Users(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
 }
