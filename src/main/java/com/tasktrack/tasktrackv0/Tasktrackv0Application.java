b6719da49f4d4a099c7bd11d37560db6b985bcd4
jason <jasonkim4201@gmail.com>
2020-10-13 12:40:57 -0400

    Initial commit: added basic spring dependencies

---
+++ src/main/java/com/tasktrack/tasktrackv0/Tasktrackv0Application.java
@@ -0,0 +1,13 @@
+package com.tasktrack.tasktrackv0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tasktrackv0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tasktrackv0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasktrack/tasktrackv0/Tasktrackv0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasktrack.tasktrackv0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tasktrackv0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
